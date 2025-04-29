fa8ccaa55f0e7765019537bc0e137acb99be3311
Karthik Gowda L <106536496+Kgl-01@users.noreply.github.com>
2025-04-29 06:19:23 +0000

    first variadic func example

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Do Somehting")
+	fmt.Println("Do Something")
+}
+
+func variadicFunctions(nums ...int) int {
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
 }
